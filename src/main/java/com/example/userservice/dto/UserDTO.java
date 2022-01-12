04cdba7ac3929ea8c6ed270cb8f7dba63b6fc686
김창대 <cdkim1226@gmail.com>
2022-01-12 16:36:18 +0900

    Create User Function

---
+++ src/main/java/com/example/userservice/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private String name;
+    private String pwd;
+    private String userId;
+    private Data createdAt;
+
+    private String encryptedPwd;
+
+}

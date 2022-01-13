897415af19f39a00e2b54ef140d92923c3799f95
김창대 <cdkim1226@gmail.com>
2022-01-13 18:53:51 +0900

    Add findByAll & findByUserId

--- src/main/java/com/example/userservice/dto/UserDTO.java
+++ src/main/java/com/example/userservice/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.example.userservice.dto;
 
+import com.example.userservice.vo.ResponseOrder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class UserDTO {
@@ -11,5 +14,7 @@ public class UserDTO {
     private Data createdAt;
 
     private String encryptedPwd;
+
+    private List<ResponseOrder> orders;
 
 }
